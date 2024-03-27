75c87fc8df7128ff22af8d15fe04abea4a010f11
salernoelia <putopet@gmail.com>
2024-03-27 21:05:13 +0100

    feat: simple api for sqlite db, capacitor.js adjustments

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,10 @@
-import { CapacitorConfig } from "@capacitor/cli";
+import type { CapacitorConfig } from "@capacitor/cli";
+
 const config: CapacitorConfig = {
   appId: "com.example.app",
   appName: "my-app",
   webDir: "dist",
   bundledWebRuntime: false,
-  server: { url: "http://10.128.136.220:3000/", cleartext: true },
+  server: { url: "http://192.168.1.13:3000/", cleartext: true },
 };
 export default config;
--- pages/index.vue
+++ pages/index.vue
@@ -10,11 +10,14 @@
       max="100"
       step="1"
     />
+    <h1>{{ data }}</h1>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useStorage } from "@vueuse/core";
+
+const data = await $fetch("api/users");
 
 let range = ref<number>(0);
 
